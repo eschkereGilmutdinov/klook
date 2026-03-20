bcc638ecaa65dc33da20bbe4ad5cd2bcd9e32daf
eschkereGilmutdinov <gilmutdinoviskander9@gmail.com>
2026-03-20 20:50:32 +0300

    delete

--- Homework1/backend/Klook.Api/Klook.Api/Models/DatasetItem.cs
+++
@@ -1,8 +0,0 @@
-namespace Klook.Api.Models;
-
-public class DatasetItem
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-}
