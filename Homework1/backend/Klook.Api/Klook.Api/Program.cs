889b7d282bead914b3ac506ffca57641fe78dcf7
eschkereGilmutdinov <gilmutdinoviskander9@gmail.com>
2026-03-20 23:42:40 +0300

    swagger

--- Homework1/backend/Klook.Api/Klook.Api/Program.cs
+++ Homework1/backend/Klook.Api/Klook.Api/Program.cs
@@ -29,11 +29,8 @@ namespace Klook.Api
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseCors("AllowFrontend");
 
